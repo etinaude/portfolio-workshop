5d9cd7aac479b37ee01604407f89fabc04f21898
Etienne <eti@naude.dev>
2022-08-30 18:46:51 +1200

    init repo

---
+++ svelte.config.js
@@ -0,0 +1,27 @@
+import preprocess from 'svelte-preprocess';
+import json from '@rollup/plugin-json'
+import adapter from '@sveltejs/adapter-netlify';
+
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
+	},
+	plugins: [
+		json({
+			compact: true
+		})]
+};
+
+export default config;
