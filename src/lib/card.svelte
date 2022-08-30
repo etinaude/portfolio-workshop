e403d7a981b0744965fde4a0ec8d33240ff0a79d
Etienne <eti@naude.dev>
2022-08-30 18:47:15 +1200

    adjust components

---
+++ src/lib/card.svelte
@@ -0,0 +1,146 @@
+<script lang="ts">
+  export let title: string;
+  export let description: string;
+  export let image_url: string;
+
+  export let follow_url: string = "https://github.com";
+
+  export let small: string = "false";
+</script>
+
+<div class="card tilt {small == 'true' ? 'small' : ''}">
+  <a href={follow_url} target="_blank" rel="noopener">
+    <div class="img">
+      <img src={"src/content/images/" + image_url} alt="project" />
+    </div>
+
+    <h3>{title}</h3>
+
+    <caption>{description}</caption>
+
+    <div class="read-more">read more →</div>
+  </a>
+</div>
+
+<style lang="scss">
+  @import "./../content/theme.scss";
+
+  .card {
+    width: 30%;
+    margin-bottom: 60px;
+    overflow: hidden;
+    border-radius: 5px;
+    background-color: var(--background-tint-color);
+    display: flex;
+    text-align: center;
+    cursor: pointer;
+    box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.2);
+
+    &:hover {
+      transform: scale(1.05);
+    }
+
+    * {
+      cursor: pointer;
+    }
+
+    a {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      overflow: hidden;
+      text-decoration: none;
+      height: 100%;
+      padding-bottom: 20px;
+      margin-bottom: auto;
+      box-sizing: border-box;
+      width: 100%;
+    }
+
+    .img {
+      position: relative;
+      width: 100%;
+      aspect-ratio: 1;
+    }
+  }
+
+  h3 {
+    color: var(--accent-color);
+    margin-bottom: 8px;
+  }
+
+  caption {
+    padding: 0px 30px;
+  }
+
+  .read-more {
+    margin-top: 10px;
+    color: var(--accent-color);
+    font-size: 0.9em;
+    display: none;
+  }
+
+  img {
+    width: 100%;
+    aspect-ratio: 1;
+    object-fit: cover;
+    position: absolute;
+    left: 0;
+  }
+
+  .card.small {
+    width: 23%;
+    .read-more {
+      display: none;
+    }
+
+    h3 {
+      color: var(--accent-color);
+      margin-bottom: 8px;
+      font-size: 1.2em;
+    }
+
+    caption {
+      padding: 0px 10px;
+    }
+  }
+
+  @media (max-width: 1100px) {
+    .card {
+      width: calc(50% - 20px);
+    }
+
+    .card.small {
+      width: calc(33% - 20px);
+    }
+  }
+
+  @media (max-width: 700px) {
+    .card {
+      width: 100%;
+      img,
+      .img {
+        aspect-ratio: 3/2;
+      }
+    }
+
+    .card.small {
+      width: calc(50% - 20px);
+
+      img,
+      .img {
+        aspect-ratio: 1;
+      }
+    }
+
+    @media (max-width: 450px) {
+      .card.small {
+        width: 100%;
+        img,
+        .img {
+          aspect-ratio: 3/2;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/lib/header.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<header>
+  <nav>
+    <ul>
+      <li class:active={$page.url.pathname === "/"}>
+        <a sveltekit:prefetch href="/">Home</a>
+      </li>
+
+      <li class:active={$page.url.pathname === "/projects"}>
+        <a sveltekit:prefetch href="/projects">Projects</a>
+      </li>
+
+      <li class:active={$page.url.pathname === "/contact"}>
+        <a href="/#cta">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<style lang="scss">
+  @import "./../content/theme.scss";
+
+  header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    width: 100vw;
+    background: var(--background-tint-color);
+    height: 50px;
+    z-index: 1000;
+  }
+
+  nav {
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    height: 100%;
+
+    a {
+      display: flex;
+      height: 100%;
+      align-items: center;
+      padding: 0 1em;
+      font-weight: 700;
+      font-size: 0.8rem;
+      text-transform: uppercase;
+      letter-spacing: 0.1em;
+      text-decoration: none;
+      transition: color 0.2s linear;
+      color: var(--accent-color);
+    }
+  }
+
+  ul {
+    position: relative;
+    padding: 0;
+    margin: 0;
+    height: 3em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+    background-size: contain;
+
+    li {
+      position: relative;
+      height: 100%;
+      &.active::before {
+        --size: 6px;
+        content: "";
+        width: 0;
+        height: 0;
+        position: absolute;
+        top: 0;
+        left: calc(50% - var(--size));
+        border: var(--size) solid transparent;
+        border-top: var(--size) solid var(--accent-color);
+      }
+    }
+  }
+
+  a:hover {
+    color: var(--accent-tint-color);
+  }
+
+  @media (max-width: 800px) {
+    nav {
+      left: 0;
+      transform: none;
+    }
+  }
+</style>
