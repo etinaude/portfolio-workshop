77671d6036e8753d0564fa5450c8e143d9e900f0
Etienne <eti@naude.dev>
2022-09-17 14:28:32 +1200

    rename templates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,98 +3,15 @@
 </script>
 
 <script lang="ts">
-  import { onMount } from "svelte";
-
-  import FlipCard from "$lib/flipCard.svelte";
-  import projects from "../content/projects.json";
-  import info from "../content/general-info.json";
 </script>
 
 <svelte:head>
-  <title>{info.firstName} {info.lastName}</title>
+  <title>GSDC Portfolio Workshop</title>
 </svelte:head>
 
-<section id="about">
+<section>
   <div class="header-text">
-    <h1>{info.firstName} {info.lastName}</h1>
-    <h2>{info.tagLine}</h2>
-
-    <div class="contacts">
-      {#each info.contacts as contact}
-        <p>
-          {contact.type}: <a href={contact.url}>{contact.displayText}</a>
-        </p>
-      {/each}
-    </div>
-  </div>
-
-  <div class="scroll">
-    <div class="down">⌄</div>
-    Scroll to see some projects
-    <div class="down">⌄</div>
+    Please Swap this file (<b>index.svelte</b>) out for one of of of the
+    templates (<b>templateX.svelte</b>)
   </div>
 </section>
-
-<h2>Projects</h2>
-
-<div class="grid">
-  {#each projects as project}
-    <FlipCard
-      title={project.title}
-      description={project.description}
-      image_url={project.image_url}
-      follow_url={project.follow_url}
-    />
-  {/each}
-</div>
-
-<style lang="scss">
-  h2 {
-    text-align: center;
-    font-weight: 400;
-  }
-
-  .grid {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    margin: 0 auto;
-  }
-
-  .scroll {
-    position: absolute;
-    bottom: 40px;
-    left: 50%;
-    transform: translateX(-50%);
-    text-align: center;
-    font-size: 1.5rem;
-    color: #fff;
-    margin-bottom: 1rem;
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    line-height: 1em;
-
-    .down {
-      margin: 0 0.5rem;
-      font-size: larger;
-      height: 1.5rem;
-      animation: bounce 1s infinite;
-    }
-
-    @keyframes bounce {
-      0% {
-        transform: translateY(0);
-      }
-      50% {
-        transform: translateY(-10px);
-      }
-      100% {
-        transform: translateY(0);
-      }
-    }
-  }
-</style>
