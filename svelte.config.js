37244367988947610efdedcfd7b35aa1361abdbf
Etienne <eti@naude.dev>
2022-09-20 21:32:35 +1200

    fixed svelte build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,11 @@ const config = {
 	kit: {
 		files: { assets: 'src/content' },
 		adapter: adapter()
-	}
+	},
+
+	paths: {
+		base: process.env.SVELTE_BASE_PATH ?? '',
+	},
 };
 
 export default config;
