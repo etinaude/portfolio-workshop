753e05c86e27eca5732e5e4b1397c6ce4c8caad9
Matt Moran <mattmoran2000@gmail.com>
2022-09-21 12:01:57 +1200

    Create links to templates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <script lang="ts">
+  import { base } from '$app/paths';
 </script>
 
 <svelte:head>
@@ -11,7 +12,12 @@
 
 <section>
   <div class="header-text">
-    Please Swap this file (<b>+page.svelte</b>) out for one of of of the
-    templates (<b>templateX.svelte</b>)
+    Please swap this file (<b>src/routes/+page.svelte</b>) out for one of the templates:
+    <ul>
+      <li><a href={`${base}/template1`}>src/routes/template1/+page.svelte</a></li>
+      <li><a href={`${base}/template2`}>src/routes/template2/+page.svelte</a></li>
+      <li><a href={`${base}/template3`}>src/routes/template3/+page.svelte</a></li>
+      <li><a href={`${base}/template4`}>src/routes/template4/+page.svelte</a></li>
+    </ul>
   </div>
 </section>
